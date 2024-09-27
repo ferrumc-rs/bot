7e699c60a44de2889578049af69dac7c8e8958af
ReCore <66930138+ReCore-sys@users.noreply.github.com>
2024-09-27 22:20:18 +0930

    a

--- src/util/git.ts
+++ src/util/git.ts
@@ -55,7 +55,7 @@ export function getMostRecentCommit() {
     }
 
     var pretty_text =
-        '--pretty=format:"[%S]: [%s](https://github.com/ferrumc-rs/ferrumc/commit/%H) - %aN | <t:%at:R>"';
+        '--pretty=format:"[%d]: [%s](https://github.com/ferrumc-rs/ferrumc/commit/%H) - %aN | <t:%at:R>"';
     var replace_regex = /\[.*origin\/(.*)\)\]/g;
     var proc = Bun.spawnSync(
         ["git", "log", "--branches='*'", "-1", pretty_text],
