c926f30d67bff2aa4fd0bdf89f638244845d40e9
ReCore <66930138+ReCore-sys@users.noreply.github.com>
2024-09-27 22:44:36 +0930

    i hate callbacks

--- src/util/git.ts
+++ src/util/git.ts
@@ -1,9 +1,10 @@
 var existsSync = require("bun:fs").existsSync;
 var fs = require("fs");
 var colorize = require("colorize");
-const { exec } = require("child_process");
+const util = require("node:util");
+const exec = util.promisify(require("child_process").exec);
 
-export function setupGit() {
+export async function setupGit() {
     console.log(
         colorize.ansify("#green[(FerrumC)] #grey[Initialising Git...]")
     );
@@ -19,61 +20,44 @@ export function setupGit() {
                 "#green[(FerrumC)] #grey[Cloning git repository...]"
             )
         );
-        exec(
-            "git clone https://github.com/ferrumc-rs/ferrumc ./git_repo",
-            function (error: Error, stdout: string, stderr: string) {
-                if (error) {
-                    console.error(
-                        colorize.ansify(
-                            "#red[(FerrumC)] #red[Failed to clone git repository]"
-                        )
-                    );
-                    console.error(stderr);
-                } else {
-                    console.log(
-                        colorize.ansify(
-                            "#green[(FerrumC)] #green[Git repository cloned]"
-                        )
-                    );
-                }
-            }
-        );
+        await exec(
+            "git clone https://github.com/ferrumc-rs/ferrumc ./git_repo"
+        ).catch((err: Error) => {
+            console.error(
+                colorize.ansify(
+                    "#red[(FerrumC)] #red[Failed to clone git repository]"
+                )
+            );
+            console.error(err);
+        });
     }
 }
 
-export function getMostRecentCommit() {
-    let output = "";
-    exec(
-        "git -C ./git_repo pull --all",
-        function (error: Error, stdout: string, stderr: string) {
-            if (error) {
-                console.error(
-                    colorize.ansify(
-                        "#red[(FerrumC)] #red[Failed to fetch git repository]"
-                    )
-                );
-                console.error(stderr);
-            }
-
-            var pretty_text =
-                '--pretty=format:"[%S]: [%s](https://github.com/ferrumc-rs/ferrumc/commit/%H) - %aN | <t:%at:R>"';
-            var replace_regex = /\[ \((.*)\)\]/g;
-            exec(
-                'git -C ./git_repo log --branches="*" -1 ' + pretty_text,
-                function (error: Error, stdout: string, stderr: string) {
-                    if (error) {
-                        console.error(
-                            colorize.ansify(
-                                "#red[(FerrumC)] #red[Failed to get most recent commit]"
-                            )
-                        );
-                        console.error(stderr);
-                    } else {
-                        output = stdout.toLocaleString().trim();
-                    }
-                }
+export async function getMostRecentCommit() {
+    var { stdout, stderr } = await exec("git -C ./git_repo pull --all").catch(
+        (err: Error) => {
+            console.error(
+                colorize.ansify(
+                    "#red[(FerrumC)] #red[Failed to fetch git repository]"
+                )
             );
+            console.error(stderr);
         }
     );
+
+    var pretty_text =
+        '--pretty=format:"[%S]: [%s](https://github.com/ferrumc-rs/ferrumc/commit/%H) - %aN | <t:%at:R>"';
+    var replace_regex = /\[ \((.*)\)\]/g;
+    var { stdout, stderr } = await exec(
+        'git -C ./git_repo log --branches="*" -1 ' + pretty_text
+    ).catch((err: Error) => {
+        console.error(
+            colorize.ansify(
+                "#red[(FerrumC)] #red[Failed to fetch git repository]"
+            )
+        );
+        console.error(stderr);
+    });
+    var output = stdout.replace(replace_regex, "$1");
     return output;
 }
