a8d7f5f9576b1d9a2160bfa47c61e9d639ea7782
ReCore <66930138+ReCore-sys@users.noreply.github.com>
2024-09-27 21:14:48 +0930

    cloning the full repo now

--- src/util/git.ts
+++ src/util/git.ts
@@ -20,7 +20,7 @@ export function setupGit() {
             )
         );
         exec(
-            "git clone --bare https://github.com/ferrumc-rs/ferrumc ./git_repo",
+            "git clone https://github.com/ferrumc-rs/ferrumc ./git_repo",
             function (error: Error, stdout: string, stderr: string) {
                 if (error) {
                     console.error(
